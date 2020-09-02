a3fef5a7e870e3445817634eaf5c9f79957566bc
sarlury <jemy.sarlury@gmail.com>
2020-09-02 23:22:24 +0700

    board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'inventory';
+export class AppComponent implements OnInit {
+
+  private roles: string[];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username: string;
+
+  constructor(
+    private tokenService: TokenStorageService
+  ) {}
+  ngOnInit() {
+    this.isLoggedIn = !!this.tokenService.getToken();
+
+    if(this.isLoggedIn) {
+      const user = this.tokenService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout() {
+    this.tokenService.signOut();
+    window.location.reload();
+  }
 }
